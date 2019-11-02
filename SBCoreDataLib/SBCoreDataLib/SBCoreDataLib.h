b9b46baa00afe3f9cf72139f1cd457ebb673b776
Soham Bhattacharjee <sohamb.1390@gmail.com>
2019-11-02 20:56:26 +0530

    - Initial commit with example target

---
+++ SBCoreDataLib/SBCoreDataLib/SBCoreDataLib.h
@@ -0,0 +1,19 @@
+//
+//  SBCoreDataLib.h
+//  SBCoreDataLib
+//
+//  Created by Soham Bhattacharjee on 02/11/19.
+//  Copyright © 2019 Soham Bhattacharjee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SBCoreDataLib.
+FOUNDATION_EXPORT double SBCoreDataLibVersionNumber;
+
+//! Project version string for SBCoreDataLib.
+FOUNDATION_EXPORT const unsigned char SBCoreDataLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBCoreDataLib/PublicHeader.h>
+
+
---
+++ SBCoreDataLib/SBCoreDataLibExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SBCoreDataLibExample
+//
+//  Created by Soham Bhattacharjee on 02/11/19.
+//  Copyright © 2019 Soham Bhattacharjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
